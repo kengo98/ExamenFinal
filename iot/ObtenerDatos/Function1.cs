bcdef15f9f894e0e10b93fb863ee686e709f247e
KENGO NAKAMURA CHIBANA <kengonc98@gmail.com>
2020-06-26 05:55:54 -0400

    terminadoo

---
+++ iot/ObtenerDatos/Function1.cs
@@ -0,0 +1,34 @@
+namespace ObtenerDatos
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Azure.WebJobs.Extensions.Http;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Logging;
+    using System.Collections.Generic;
+    using ExamenFinalTriggerIot.Helpers;
+    using ExamenFinalTriggerIot.Models;
+
+    public static class ObtenerDatos
+    {
+        [FunctionName("ObtenerDatos")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: Constantes.COSMOS_DB_DATABASE_NAME,
+                collectionName: Constantes.COSMOS_DB_CONTAINER_NAME,
+                ConnectionStringSetting = "StrCosmos",
+                SqlQuery ="SELECT top 10 * FROM c order by c._ts desc")] IEnumerable<TempHum> datos,
+            ILogger log)
+
+        {
+            if (datos == null)
+            {
+                return new NotFoundResult();
+            }
+
+            return new OkObjectResult(datos);
+        }
+    }
+}
---
+++ iot/ObtenerDatos/Helpers/Constantes.cs
@@ -0,0 +1,11 @@
+﻿namespace ExamenFinalTriggerIot.Helpers
+{
+    public static class Constantes
+    {
+        public const string COSMOS_DB_CONNECTION_STRING = "AccountEndpoint=https://proyectoparcialuno.documents.azure.com:443/;AccountKey=2zXH8cTQQFoMgiBEeIV0ieUZlBhxoKGP5WBFrsCn60yydElGiMogyE4QqzEydnTTB0QVeSSDbbqtuuRmf8NuyQ==;";
+        public const string COSMOS_DB_DATABASE_NAME = "dbparcial";
+        public const string COSMOS_DB_CONTAINER_NAME = "temperaturaHumedad";
+        public const string COSMOS_DB_ACCOUNT_NAME = "proyectoparcialuno";
+        public const string COSMOS_DB_PRIMMARY_KEY = "2zXH8cTQQFoMgiBEeIV0ieUZlBhxoKGP5WBFrsCn60yydElGiMogyE4QqzEydnTTB0QVeSSDbbqtuuRmf8NuyQ==";
+    }
+}
